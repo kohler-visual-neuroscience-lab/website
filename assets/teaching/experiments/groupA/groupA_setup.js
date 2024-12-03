3be5938f4725fbe25f4074355ab0f07e8c6a3793
pjkohler <pjkohl3r@gmail.com>
2024-12-03 12:09:22 -0500

    group A final version

--- assets/teaching/experiments/groupA/groupA_setup.js
+++ assets/teaching/experiments/groupA/groupA_setup.js
@@ -1,34 +1,87 @@
 const neg_scenes = [
-  'assets/images/neg_scenes/cemetery02.jpg',
-  'assets/images/neg_scenes/desert01.jpg'
+  'assets/images/neg_scenes/cemetery01.jpg',
+  'assets/images/neg_scenes/desert02.jpg',
+  'assets/images/neg_scenes/destruction03.jpg',
+  'assets/images/neg_scenes/destruction04.jpg',
+  'assets/images/neg_scenes/destruction05.jpg',
+  'assets/images/neg_scenes/destruction06.jpg',
+  'assets/images/neg_scenes/destruction07.jpg',
+  'assets/images/neg_scenes/destruction08.jpg',
+  'assets/images/neg_scenes/explotion09.jpg',
+  'assets/images/neg_scenes/fire10.jpg',
+  'assets/images/neg_scenes/fire11.jpg',
+  'assets/images/neg_scenes/fire12.jpg',
+  'assets/images/neg_scenes/fire13.jpg',
+  'assets/images/neg_scenes/fire14.jpg',
+  'assets/images/neg_scenes/flood15.jpg',
+  'assets/images/neg_scenes/garbage16.jpg',
+  'assets/images/neg_scenes/graveyard17.jpg',
+  'assets/images/neg_scenes/jail18.jpg',
+  'assets/images/neg_scenes/planecrash19.jpg',
+  'assets/images/neg_scenes/pollution20.jpg',
+  'assets/images/neg_scenes/railroad21.jpg',
+  'assets/images/neg_scenes/street22.jpg',
+  'assets/images/neg_scenes/street23.jpg',
+  'assets/images/neg_scenes/street24.jpg',
+  'assets/images/neg_scenes/thunderstorm25.jpg',
+  'assets/images/neg_scenes/thunderstorm26.jpg',
+  'assets/images/neg_scenes/tornado27.jpg',
+  'assets/images/neg_scenes/tornado28.jpg',
+  'assets/images/neg_scenes/tornado29.jpg',
+  'assets/images/neg_scenes/war30.jpg' 
 ]
 const pos_scenes = [
   'assets/images/pos_scenes/beach01.jpg',
-  'assets/images/pos_scenes/beach02.jpg'
+  'assets/images/pos_scenes/beach02.jpg',
+  'assets/images/pos_scenes/firework03.jpg',
+  'assets/images/pos_scenes/sunflower28.jpg',
+  'assets/images/pos_scenes/rainbow24.jpg',
+  'assets/images/pos_scenes/rainbow25.jpg',
+  'assets/images/pos_scenes/grass05.jpg',
+  'assets/images/pos_scenes/lake19.jpg',
+  'assets/images/pos_scenes/lake18.jpg',
+  'assets/images/pos_scenes/sailing26.jpg',
+  'assets/images/pos_scenes/lake13.jpg',
+  'assets/images/pos_scenes/lake12.jpg',
+  'assets/images/pos_scenes/lake20.jpg',
+  'assets/images/pos_scenes/lake21.jpg',
+  'assets/images/pos_scenes/volcano30.jpg',
+  'assets/images/pos_scenes/lake14.jpg',
+  'assets/images/pos_scenes/lake15.jpg',
+  'assets/images/pos_scenes/lake09.jpg',
+  'assets/images/pos_scenes/lake08.jpg',
+  'assets/images/pos_scenes/firework04.jpg',
+  'assets/images/pos_scenes/penguin23.jpg',
+  'assets/images/pos_scenes/lake10.jpg',
+  'assets/images/pos_scenes/lake11.jpg',
+  'assets/images/pos_scenes/sunset29.jpg',
+  'assets/images/pos_scenes/lake07.jpg',
+  'assets/images/pos_scenes/sunflower27.jpg',
+  'assets/images/pos_scenes/happypose06.jpg',
+  'assets/images/pos_scenes/lake17.jpg',
+  'assets/images/pos_scenes/lake16.jpg',
+  'assets/images/pos_scenes/lake22.jpg'
 ]
-const neg_faces = [
-  'assets/images/faces_smaller/id01_angry.jpg',
-  'assets/images/faces_smaller/id02_angry.jpg',
-  'assets/images/faces_smaller/id03_angry.jpg'
-]
-const pos_faces = [
-  'assets/images/faces_smaller/id01_happy.jpg',
-  'assets/images/faces_smaller/id02_happy.jpg',
-  'assets/images/faces_smaller/id03_happy.jpg'
-]
+
+let neg_faces = [ ]
+let pos_faces = [ ]
+for (let i = 1; i <= 10; i++) {
+  i = i.toString();
+  while (i.length < 2) i = "0" + i;
+  neg_faces.push('assets/images/faces_smaller/id' + i + '_angry.jpg')
+  pos_faces.push('assets/images/faces_smaller/id' + i + '_happy.jpg')
+}
 
 const save_name = 'groupA_data_idxxxx.csv'
 
-
-exp_repeat_trials = 1;
-prac_repeat_trials = 1;                       // how many times to repeat each trial type
-exp_trial_num = 100;
+exp_trial_num = 160;
 prac_trial_num = 10;                       // how many times to repeat each trial type
 
-const block_number = 4;                          // how many blocks to include in the experiment 
+const block_number = 8;                          // how many blocks to include in the experiment 
                                                   // message to display before each trial.
                                                   // note: '\n\n' is needed at the end of each line 
                                                   // because prompt is displayed using the psychophysics plugin
+
 const block_len = Math.floor(exp_trial_num/block_number) // multiply by 2 because both target absent and target present trials
 
 const fixation_img = 'assets/images/fixation.png'
