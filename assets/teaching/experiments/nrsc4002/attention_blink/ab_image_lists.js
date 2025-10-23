2ccb209571afee62fc0ec5c7cc26fbb55c9dc43e
pjkohler <pjkohl3r@gmail.com>
2025-10-23 14:36:20 -0400

    attentional blink experiment

---
+++ assets/teaching/experiments/nrsc4002/attention_blink/ab_image_lists.js
@@ -0,0 +1,89 @@
+// image lists for the AB RSVP experiment
+
+const shape_targets = [
+  "shape_stimuli/targets/shape_bear002.png",
+  "shape_stimuli/targets/shape_cat009.png"
+];
+
+const shape_distractors = [
+  "shape_stimuli/distractors/shape_elephant001.png",
+  "shape_stimuli/distractors/shape_elephant002.png",
+  "shape_stimuli/distractors/shape_elephant003.png",
+  "shape_stimuli/distractors/shape_elephant004.png",
+  "shape_stimuli/distractors/shape_elephant005.png",
+  "shape_stimuli/distractors/shape_elephant006.png",
+  "shape_stimuli/distractors/shape_frog001.png",
+  "shape_stimuli/distractors/shape_frog003.png",
+  "shape_stimuli/distractors/shape_frog004.png",
+  "shape_stimuli/distractors/shape_frog006.png",
+  "shape_stimuli/distractors/shape_frog008.png",
+  "shape_stimuli/distractors/shape_frog012.png",
+  "shape_stimuli/distractors/shape_lizard002.png",
+  "shape_stimuli/distractors/shape_lizard003.png",
+  "shape_stimuli/distractors/shape_lizard005.png",
+  "shape_stimuli/distractors/shape_lizard007.png",
+  "shape_stimuli/distractors/shape_lizard009.png",
+  "shape_stimuli/distractors/shape_lizard012.png",
+  "shape_stimuli/distractors/shape_rabbit001.png",
+  "shape_stimuli/distractors/shape_rabbit002.png",
+  "shape_stimuli/distractors/shape_rabbit004.png",
+  "shape_stimuli/distractors/shape_rabbit005.png",
+  "shape_stimuli/distractors/shape_rabbit009.png",
+  "shape_stimuli/distractors/shape_rabbit011.png",
+  "shape_stimuli/distractors/shape_tiger001.png",
+  "shape_stimuli/distractors/shape_tiger003.png",
+  "shape_stimuli/distractors/shape_tiger004.png",
+  "shape_stimuli/distractors/shape_tiger006.png",
+  "shape_stimuli/distractors/shape_tiger008.png",
+  "shape_stimuli/distractors/shape_tiger010.png",
+  "shape_stimuli/distractors/shape_wolf001.png",
+  "shape_stimuli/distractors/shape_wolf004.png",
+  "shape_stimuli/distractors/shape_wolf005.png",
+  "shape_stimuli/distractors/shape_wolf007.png",
+  "shape_stimuli/distractors/shape_wolf009.png",
+  "shape_stimuli/distractors/shape_wolf012.png"
+];
+
+const texture_targets = [
+  "texture_stimuli/targets/texture_leaf005.png",
+  "texture_stimuli/targets/texture_leather004.png"
+];
+
+const texture_distractors = [
+  "texture_stimuli/distractors/texture_bark001.png",
+  "texture_stimuli/distractors/texture_bark004.png",
+  "texture_stimuli/distractors/texture_bark005.png",
+  "texture_stimuli/distractors/texture_bark009.png",
+  "texture_stimuli/distractors/texture_bark010.png",
+  "texture_stimuli/distractors/texture_bark012.png",
+  "texture_stimuli/distractors/texture_bush001.png",
+  "texture_stimuli/distractors/texture_bush003.png",
+  "texture_stimuli/distractors/texture_bush004.png",
+  "texture_stimuli/distractors/texture_bush006.png",
+  "texture_stimuli/distractors/texture_bush014.png",
+  "texture_stimuli/distractors/texture_bush017.png",
+  "texture_stimuli/distractors/texture_fabric011.png",
+  "texture_stimuli/distractors/texture_fabric015.png",
+  "texture_stimuli/distractors/texture_fabric022.png",
+  "texture_stimuli/distractors/texture_fabric040.png",
+  "texture_stimuli/distractors/texture_fabric057.png",
+  "texture_stimuli/distractors/texture_fabric076.png",
+  "texture_stimuli/distractors/texture_flower002.png",
+  "texture_stimuli/distractors/texture_flower005.png",
+  "texture_stimuli/distractors/texture_flower007.png",
+  "texture_stimuli/distractors/texture_flower008.png",
+  "texture_stimuli/distractors/texture_flower009.png",
+  "texture_stimuli/distractors/texture_flower011.png",
+  "texture_stimuli/distractors/texture_food002.png",
+  "texture_stimuli/distractors/texture_food003.png",
+  "texture_stimuli/distractors/texture_food005.png",
+  "texture_stimuli/distractors/texture_food007.png",
+  "texture_stimuli/distractors/texture_food008.png",
+  "texture_stimuli/distractors/texture_food009.png",
+  "texture_stimuli/distractors/texture_gravel005.png",
+  "texture_stimuli/distractors/texture_gravel006.png",
+  "texture_stimuli/distractors/texture_gravel010.png",
+  "texture_stimuli/distractors/texture_gravel013.png",
+  "texture_stimuli/distractors/texture_gravel017.png",
+  "texture_stimuli/distractors/texture_gravel018.png"
+];
